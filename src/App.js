0b96b6036a747162673692f3a143ae9e8c3590c3
jose1up <josealcaraz022@gmail.com>
2022-03-31 04:41:13 -0300

    CRUD of cart in react no css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./components/Cart";
 import Home from "./components/home";
-import ProductDetail from './components/productDetail';
-
+import ProductDetail from "./components/productDetail";
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route exact path='/details/:id' element={<ProductDetail/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/details/:id" element={<ProductDetail />} />
+        <Route exact path="/cart" element={<Cart />} />
       </Routes>
     </React.Fragment>
   );
---
+++ src/components/ProductCart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { removeOneFromCart } from "../redux/action";
+import { useDispatch } from "react-redux";
+import { useToast } from "@chakra-ui/react";
+
+export default function ProductCart({ id, title, image, price, quantity }) {
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const handleButoon = (e) => {
+    dispatch(removeOneFromCart(id));
+    toast({
+      title: "Deleted item.",
+      description: "The item was deleted successfully.",
+      status: "error",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+  return (
+    <>
+      <img src={image} alt={title}></img>
+      <h2>title:{title}</h2>
+      <p>price: {price}</p>
+      <p>quantity: {quantity}</p>
+      <DeleteIcon onClick={(e) => handleButoon(e)} />
+    </>
+  );
+}
